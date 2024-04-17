5b1c769846ac0dbb00edd0d32e9cc0a359e21d28
Joao <jmmsp2003@hotmail.com>
2024-04-17 01:04:30 -0300

    reparando arquivo vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// eslint-disable-next-line no-undef
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    // eslint-disable-next-line no-undef
     alias: [{ find: "@", replacement: path.resolve(__dirname, "/src") }],
   },
 });
