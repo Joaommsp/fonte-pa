3fb56af2e53bb5a96f3770ad345ab24e6c55b744
Botão <jmmsp2003@hotmail.com>
2024-04-18 09:53:59 -0300

    erro MIME 03

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()],
-    base: './',
+    plugins: [react()]
   }
 
   if (command !== 'serve') {
