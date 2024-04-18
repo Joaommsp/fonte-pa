fd3bcc8dd65cf1ec06789a951a8b1e0586433b4b
Joao <jmmsp2003@hotmail.com>
2024-04-18 15:54:34 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '.',
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/fonte-paulo-afonso/";
   }
 
-  if (command !== 'serve') {
-    config.base = '/fonte-paulo-afonso/'
-  }
-
-  return config
-})
+  return config;
+});
